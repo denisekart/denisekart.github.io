00e8c39d45efdd031015e0ad127faed859c22629
Denis Ekart <denis.ekart@pinna.si>
2019-04-22 21:42:41 +0200

    'added basic resume page with example'

--- src/personal/src/app/components/resume/resume.component.ts
+++ src/personal/src/app/components/resume/resume.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.css']
+  styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent implements OnInit {
-
+//https://codepen.io/mathiesjanssen/pen/ggeBKm
   constructor() { }
 
   ngOnInit() {
